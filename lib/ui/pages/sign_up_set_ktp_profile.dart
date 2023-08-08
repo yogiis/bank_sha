fd96f1e014f231be0633a20c0c4a4c06cfe02953
Yogi is ariyanto <yogi.ariyanto@flip.id>
2023-08-08 22:57:33 +0700

    slicing sign up veriry id card

--- lib/ui/pages/sign_up_set_ktp_profile.dart
+++ lib/ui/pages/sign_up_set_ktp_profile.dart
@@ -4,8 +4,8 @@ import '../../shared/theme.dart';
 import '../widgets/buttons.dart';
 import '../widgets/forms.dart';
 
-class SignUpSetProfilePage extends StatelessWidget {
-  const SignUpSetProfilePage({super.key});
+class SignUpSetKtpPage extends StatelessWidget {
+  const SignUpSetKtpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +31,7 @@ class SignUpSetProfilePage extends StatelessWidget {
             )),
           ),
           Text(
-            'Join Us to Unlock\nYour Growth',
+            'Verify Your\nAccount',
             style: blackTextStyle.copyWith(
               fontSize: 20,
               fontWeight: semiBold,
@@ -49,63 +49,59 @@ class SignUpSetProfilePage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                // Container(
-                //   width: 120,
-                //   height: 120,
-                //   decoration: BoxDecoration(
-                //     shape: BoxShape.circle,
-                //     color: lightBackgroundColor,
-                //   ),
-                //   child: Center(
-                //     child: Image.asset(
-                //       'assets/ic_upload.png',
-                //       width: 32,
-                //     ),
-                //   ),
-                // ),
                 Container(
                   width: 120,
                   height: 120,
-                  decoration: const BoxDecoration(
+                  decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        'assets/img_profile.png',
-                      ),
+                    color: lightBackgroundColor,
+                  ),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/ic_upload.png',
+                      width: 32,
                     ),
                   ),
                 ),
+                // Container(
+                //   width: 120,
+                //   height: 120,
+                //   decoration: const BoxDecoration(
+                //     shape: BoxShape.circle,
+                //     image: DecorationImage(
+                //       fit: BoxFit.cover,
+                //       image: AssetImage(
+                //         'assets/img_profile.png',
+                //       ),
+                //     ),
+                //   ),
+                // ),
                 const SizedBox(
                   height: 16,
                 ),
                 Text(
-                  'Shayna Hanna',
+                  'Passport/ID Card',
                   style: blackTextStyle.copyWith(
                     fontSize: 18,
                     fontWeight: medium,
                   ),
                 ),
                 SizedBox(
-                  height: 30,
-                ),
-                CustomFormField(
-                  title: 'Set PIN (6 digit number)',
+                  height: 50,
                 ),
                 const SizedBox(
                   height: 30,
                 ),
                 CustomFilledButton(
                   title: 'Continue',
-                  onPressed: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/sign-up-set-ktp',
-                    );
-                  },
+                  onPressed: () {},
                 ),
                 const SizedBox(
                   height: 50,
+                ),
+                CustomTextButton(
+                  title: 'Skip for Now',
+                  onPressed: () {},
                 ),
               ],
             ),
